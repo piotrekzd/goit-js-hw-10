af00dcf89b73bd79a673f784b42a1e275c8cd763
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-08-14 21:25:05 +0200

    fetchCountries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
