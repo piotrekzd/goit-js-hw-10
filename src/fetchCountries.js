24cf40531aaad557fef032991a583b4490c64416
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-08-16 11:30:32 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,5 @@
 function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,populations,flags,languages`)
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(response.status);
